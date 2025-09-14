af20380eb83efe48e7ab772e5d983afdfe5ebf6b
Antony Reis <antony.matheus@eyeflow.ai>
2025-09-14 16:05:37 -0300

    feat: created POO structure for v3 implementation

--- v3/main.py
+++ v3/main.py
@@ -1,210 +1,74 @@
-menu = """
+from abc import ABC, abstractmethod, abstractproperty
+from datetime import datetime
 
-[d] - Depositar Valor
-[s] - Sacar Valor
-[e] - Imprimir Extrato
-[u] - Cadastrar Usuário
-[c] - Criar Nova Conta
-[l] - Listagem
-[q] - Sair
+class Conta():
+    def __init__(self, saldo, numero_conta, agencia, cliente, historico):
+        self._saldo = saldo
+        self._numero_conta = numero_conta
+        self._agencia = agencia
+        self._cliente = cliente
+        self._historico = historico
 
-"""
-
-menu_listas = """
-          
-O que você deseja listar?
-        
-[1] - Listar Usuários
-[2] - Listar Contas
-[3] - Voltar para Menu
+    @property
+    def saldo(self):
+        return self._saldo
     
-"""
-
-saldo = 0
-limite_valor = 500
-saques_realizados = 0
-
-extrato = []
-usuarios = []
-contas = []
-
-LIMITE_SAQUES = 3
-
-def cria_usuario():
-    cpf = input("Digite o CPF do novo usuário: ").replace(".", "").replace("-", "")
-    has_usuario = any(cpf == usuario_existente["cpf"] for usuario_existente in usuarios)
-
-    if has_usuario:
-        print(f"Não é possível cadastrar outro usuário com mesmo CPF.")
-        return ""
-
-    else:
-        nome = input("Digite o nome do novo usuário: ")
-        data_nascimento = input("Digite a data de nascimento do novo usuário: ")
-        endereco = input("Digite o nome do novo usuário:\n(LOGRADOURO - NÚMERO - BAIRRO - CIDADE - ESTADO) \n")
-        
-
-        novo_usuario = { 
-            "nome": nome, 
-            "data_nascimento": data_nascimento, 
-            "cpf": cpf, 
-            "endereco": endereco 
-        }
-        usuarios.append(novo_usuario)
-
-        print(f"Usuário {nome.title()} criado com sucesso.")
-        return ""
-
-def cria_conta():
-
-    cpf_limpo = input("Digite o CPF do usuário dessa nova conta: ").replace(".","").replace("-","")
-    usuario = next(
-        (usuario_existente for usuario_existente in usuarios if usuario_existente["cpf"] == cpf_limpo),
-        None
-    )
-
-    if not usuario:
-        print(f"Nenhum usuário encontrado com o CPF inserido.")
-        return ""
-
-    else:
-        conta = {
-            "agencia": "0001", 
-            "numero_conta": len(contas) + 1, 
-            "usuario": usuario
-        }
-
-        contas.append(conta)
-        print(f"Conta {conta['agencia']}-{conta['numero_conta']} criada para usuário {usuario['nome']} com sucesso.")
-        return ""
-
-def lista() -> None:
-
-    def lista_usuario():
-        print(f"==== LISTA DE USUÁRIOS ====")
-        if len(usuarios) > 0:
-            for index, usuario in enumerate(usuarios, start=1):
-                print(f"{index}. Nome: {usuario['nome']}, CPF: {usuario['cpf']}, Data de Nascimento: {usuario['data_nascimento']}, Endereço: {usuario['endereco']}\n")
-            return ""
-        
-        else:
-            return f"Nenhuma usuário cadastrado."
-        
-    def lista_conta():
-        print(f"==== LISTA DE CONTAS ====")
-        if len(contas) > 0:
-            for index, conta in enumerate(contas, start=1):
-                print(f"{index}. Agência: {conta['agencia']}, Número da Conta: {conta['numero_conta']}, Usuário: {conta['usuario']['nome']}\n")
-            return ""
-        else:
-            return f"Nenhuma conta cadastrada."
-        
-    print(menu_listas)
-    opcao = input("Digite a opção que você deseja: ")
-    OPCOES_VALIDAS = {
-        '1': lista_usuario,
-        '2': lista_conta,
-        '3': "VOLTAR"
-    }
-    if opcao not in OPCOES_VALIDAS:
-        return "Opção de listagem inválida, voltando para menu principal.\n"
+    @classmethod
+    def nova_conta(cls, cliente, numero_conta):
+        return cls()
     
-    elif opcao == '3':
-        return "Voltando para menu principal...\n"
-
-    else: 
-        return OPCOES_VALIDAS[opcao]() 
+    def sacar(self, valor):
+        return bool
     
-def depositar(saldo, valor, /):
-    if valor <= 0:
-            return("Valor depositado inválido.")
-    
-    saldo += valor
-    atualiza_extrato(saldo, valor, op="Depósito")
+    def depositar(self, valor):
+        return bool
 
-    return saldo, f"""
-            Operação realizada com sucesso!
-            Saldo Atualizado: R$ {saldo:.2f}
-            """
+class ContaCorrente(Conta):
+    def __init__(self, saldo, numero_conta, agencia, cliente, historico, limite, limite_saques):
+        super().__init__(saldo, numero_conta, agencia, cliente, historico)
+        self._limite = limite
+        self._limite_saques = limite_saques
 
-def sacar(*, saldo=0.0, valor=0.0):
-    global limite_valor, saques_realizados, LIMITE_SAQUES
-
-    if saldo <= 0:
-        return saldo, "Não há valor disponível para saque."
-    
-    if saques_realizados == LIMITE_SAQUES:
-        return saldo, "Limite de saques excedido, tente novamente outro dia."
-
-    if valor > limite_valor:
-        return saldo, f"Valor inserido inválido. Seu limite de saque é R$ {limite_valor:.2f}."
-
-    elif valor > saldo:
-        return saldo, f"Valor indisponível na conta. Saldo disponível: {saldo:.2f}"
-
-    saldo -= valor
-    saques_realizados += 1
-    atualiza_extrato(saldo, valor, op="Saque")
-
-    return saldo, f"""
-            Operação realizada com sucesso!
-            Saldo Atualizado: R$ {saldo:.2f}
-            """
-
-def atualiza_extrato(saldo, valor, /, op="Transação"):
-    extrato.append(f"{op} no valor de R$ {valor:.2f}. Saldo final: R$ {saldo:.2f}")
-
-def imprime_extrato():
-    print("==== EXTRATO BANCÁRIO ====", end="\n")
-
-    for operacao in extrato:
-        print(f"{operacao}")
-
-    return f"\nSaldo: R$ {saldo:.2f}"
-
-def sair():
-    return """
-        Obrigado por ser nosso cliente hoje!
-        Até a próxima!
-        """
-
-def main():
-    print("Seja bem vindo ao nosso sistema bancário! \n")
-    while True:
-        global saldo
-        print(menu)
-        opcao = input("Digite a opção que deseja selecionar: ")
-
-        OPCOES_VALIDAS = {
-            "d": depositar,
-            "s": sacar,
-            "e": imprime_extrato,
-            "u": cria_usuario,
-            "c": cria_conta,
-            "l": lista,
-            "q": sair
-        }
-
-        if opcao.lower() not in OPCOES_VALIDAS.keys():
-            print("Opção Inválida! Por favor selecione uma das opções disponíveis.")
-            continue
-
-        if opcao.lower() == "q":
-            print(OPCOES_VALIDAS[opcao.lower()]())
-            break
-
-        elif opcao.lower() == "s":
-            valor = float(input("Digite o valor que deseja sacar: "))
-            saldo, msg = OPCOES_VALIDAS[opcao.lower()](saldo=saldo, valor=valor)
-            print(msg)
-            continue
+class Cliente():
+    def __init__(self, endereco):
+        self._endereco = endereco
+        self._contas = []
         
-        elif opcao.lower() == "d":
-            valor = float(input("Digite o valor que deseja depositar: "))
-            saldo, msg = OPCOES_VALIDAS[opcao.lower()](saldo, valor)
-            print(msg)
-            continue
+    def realizar_transacao(self, conta, transacao):
+        pass
+    
+    def adicionar_conta(self, conta):
+        pass
 
-        print(OPCOES_VALIDAS[opcao.lower()]())
+class PessoaFisica(Cliente):
+    def __init__(self, endereco, cpf, nome, data_nascimento):
+        super().__init__(endereco)
+        self._cpf = cpf
+        self._nome = nome
+        self._data_nascimento = data_nascimento
 
-main()
+class Transacao(ABC):
+    @abstractmethod
+    def registrar(self, conta):
+        pass
+        
+class Deposito(Transacao):
+    def __init__(self, valor):
+        self._valor = valor
+        
+    def registrar(self, conta):
+        pass
+
+class Saque(Transacao):
+    def __init__(self, valor):
+        self._valor = valor
+        
+    def registrar(self, conta):
+        pass
+    
+class Historico:
+    def __init__(self):
+        self._transacoes = []
+        
+    def adicionar_transacao(self, transacao):
+        pass
