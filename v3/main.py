ee709df969b9a723c79816e018485c6ddc20e711
Antony Reis <antony.matheus@eyeflow.ai>
2025-09-14 18:38:03 -0300

    feat!: POO structure now completed, logic of bank system created

--- v3/main.py
+++ v3/main.py
@@ -2,52 +2,101 @@ from abc import ABC, abstractmethod, abstractproperty
 from datetime import datetime
 
 class Conta():
-    def __init__(self, saldo, numero_conta, agencia, cliente, historico):
-        self._saldo = saldo
+    def __init__(self, numero_conta, cliente):
+        self._saldo = 0
         self._numero_conta = numero_conta
-        self._agencia = agencia
+        self._agencia = "0001"
         self._cliente = cliente
-        self._historico = historico
+        self._historico = Historico()
 
     @property
     def saldo(self):
         return self._saldo
     
+    @property
+    def numero_conta(self):
+        return self._numero_conta
+    
+    @property
+    def agencia(self):
+        return self._agencia
+    
+    @property
+    def cliente(self):
+        return self._cliente
+    
+    @property
+    def historico(self):
+        return self._historico
+    
     @classmethod
     def nova_conta(cls, cliente, numero_conta):
-        return cls()
+        return cls(numero_conta, cliente)
     
     def sacar(self, valor):
-        return bool
+        saldo = self.saldo
+        sucesso = True
+        if saldo <= 0 or valor > saldo:
+            sucesso = False
+        else: 
+            self._saldo -= valor
+        
+        return sucesso
     
     def depositar(self, valor):
-        return bool
+        sucesso = True
+        if valor <= 0:
+            sucesso = False
+        else: 
+            self._saldo += valor
+        
+        return sucesso
 
 class ContaCorrente(Conta):
-    def __init__(self, saldo, numero_conta, agencia, cliente, historico, limite, limite_saques):
-        super().__init__(saldo, numero_conta, agencia, cliente, historico)
-        self._limite = limite
-        self._limite_saques = limite_saques
+    def __init__(self, numero_conta, cliente, limite=500, limite_saques=3):
+        super().__init__(numero_conta, cliente)
+        self.limite = limite
+        self.limite_saques = limite_saques
+    
+    def sacar(self, valor):
+        saques_realizados = len([transacao for transacao in self.historico.transacoes if transacao["Operação"] == Saque.__name__])
+        
+        if valor > self.limite or saques_realizados >= self.limite_saques:
+            return False
+        
+        return super().sacar(valor)
+    
+    def __str__(self):
+        return f"""
+            Agência:\t {self.agencia}
+            C\C:t\t\ {self.numero_conta}
+            Titular:\t {self.cliente.nome}
+        """
 
 class Cliente():
     def __init__(self, endereco):
-        self._endereco = endereco
-        self._contas = []
+        self.endereco = endereco
+        self.contas = []
         
     def realizar_transacao(self, conta, transacao):
-        pass
+        transacao.registrar(conta)
     
     def adicionar_conta(self, conta):
-        pass
+        self.contas.append(conta)
 
 class PessoaFisica(Cliente):
     def __init__(self, endereco, cpf, nome, data_nascimento):
         super().__init__(endereco)
-        self._cpf = cpf
-        self._nome = nome
-        self._data_nascimento = data_nascimento
+        self.cpf = cpf
+        self.nome = nome
+        self.data_nascimento = data_nascimento
 
 class Transacao(ABC):
+    @property
+    @abstractproperty
+    def valor(self):
+        pass
+    
     @abstractmethod
     def registrar(self, conta):
         pass
@@ -56,19 +105,41 @@ class Deposito(Transacao):
     def __init__(self, valor):
         self._valor = valor
         
+    @property
+    def valor(self):
+        return self._valor
+    
     def registrar(self, conta):
-        pass
+        sucesso = conta.depositar(self.valor)
+        
+        if sucesso:
+            conta.historico.adicionar_transacao(self)
 
 class Saque(Transacao):
     def __init__(self, valor):
         self._valor = valor
         
+    @property
+    def valor(self):
+        return self._valor
+    
     def registrar(self, conta):
-        pass
+        sucesso = conta.sacar(self.valor)
+        
+        if sucesso:
+            conta.historico.adicionar_transacao(self)
     
 class Historico:
     def __init__(self):
         self._transacoes = []
         
+    @property
+    def transacoes(self):
+        return self._transacoes
+        
     def adicionar_transacao(self, transacao):
-        pass
+        self._transacoes.append({
+            "Operação": transacao.__class__.__name__,
+            "Valor": transacao.valor,
+            "Horário": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        })
